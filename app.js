90227e9931ebd1f5fdb935d47d0b76e6fc98950d
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-06-22 08:07:57 +0500

    js

---
+++ app.js
@@ -0,0 +1,50 @@
+const USERS = [
+    {
+        name: "John",
+        age: 32,
+        gender: "male",
+        nick
+    },
+    {
+        name: "Laylo",
+        age: 19,
+        gender: "female",
+    },
+    {
+        name: "Selena",
+        age: 29,
+        gender: "female",
+    }
+
+]
+    // for(let index = 0, len = USERS.length; index < len; index++) {
+    //     if (USERS[index].age < 20) {
+    //         console.log(USERS[index].name);
+    //     }
+        
+    // }
+     
+    // for (let index = 0, len = USERS.length; index < len; index++) {
+    //     if (USERS[index].gender === "female" ) {
+    //         console.log(USERS[index].name);
+    //     }
+        
+    // }
+    // for (let index = 0, len = USERS.length; index < len; index++) {
+    //     if (USERS[index].gender === "male" ) {
+    //         if (USERS[index].isMarried === false) {
+    //             console.log(USERS[index].name);
+    //         }
+    //     }
+        
+    // }
+
+
+    //1."janob" va "xonim" so'zini mosini qo'shib chiqaring
+    function mrMs() {
+        
+    }
+    console.log(mrMs({name:Akmal, gender:male}));// janob Akmal
+    console.log(mrMs({name:Laylo, gender:female}));// Xonim Laylo
+    console.log(mrMs({name:Sarvar, gender:male}));// janob Sarvar
+    
