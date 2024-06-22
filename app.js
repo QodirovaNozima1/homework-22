ae960b22c1bf6417bf4afab9fbb2a31b711ba3de
QodirovaNozima1 <nozimaqodirova07@gmail.com>
2024-06-22 08:28:21 +0500

    js

--- app.js
+++ app.js
@@ -48,3 +48,46 @@ const USERS = [
     console.log(mrMs({name:Laylo, gender:female}));// Xonim Laylo
     console.log(mrMs({name:Sarvar, gender:male}));// janob Sarvar
     
+    //2. Agar ikkinchi argument false bo'lsa carta raqami yashirilsin aks holda aksi
+    function card(params) {
+        
+    }
+    console.log(card("8600 1234 5672 4524",true));//8600 1234 5672 4524
+    console.log(card("8600 4321 7535 9324",false));//8600 **** **** **24
+    console.log(card("8600 4524 1234 5624 ",false));//8600 **** **** **24
+
+    //3.Perimetrni qaytaradigan function tuzing
+    function perimertRect() {
+        
+    }
+
+    console.log(perimertRect(6,2));
+
+
+    //4."ha" ga true "yo'q" ga false qaytaring
+    function yesNo() => {
+        
+    }
+    console.log(yesNo("ha"));//true
+
+
+    //5. agar + bo'lmasa telefon numberga + ni qo'wib qaytaring
+    function telNumber() {
+        
+    }
+    console.log("+998 99 902 00 65");
+    console.log("998 99 902 00 65");//+
+
+    //6.Decloration functionni arraw ustuniga o'giring va bir qatorda yozing
+    function greeting() {
+        return "Salom"
+    }
+    console.log(greeting());
+
+
+    //7."dark" kirsa "light" qaytaring yoki aksi "light" kirsa "dark" qaytaring
+    function brightness() {
+        
+    }
+    console.log(brightness("dark"));//light
+    console.log(brightness("light"));//dark
